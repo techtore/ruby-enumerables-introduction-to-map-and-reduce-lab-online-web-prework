0d203ab4047046027bb68eeebcc61e1734c4ab06
Victoria Hale <vic.hale74@gmail.com>
2019-09-30 18:09:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,7 +63,7 @@ def reduce_to_all_true(source_array)
   i = 0
   
   while i < source_array.size do 
-    if source_array == false
+    if source_array.all?{false} 
     return false
     
     i += 1
